3403785e776cfcba951fc21a1797c64325f652ad
Atala <159235635+atalakeanue@users.noreply.github.com>
2025-10-17 19:33:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 echo "<h1>Web1 - Aplikasi 1</h1>";
 echo "<p> diedit dari github </p>";
 echo "<p>Server IP: " . $_SERVER['SERVER_ADDR'] . " | Port: 2000</p>";
-echo "<p>Saya Atala mengakses Web ini pada "</p>";
+echo "<p>Saya Atala mengakses Web ini pada </p>";
 echo "<p>Waktu server: " . date('Y-m-d H:i:s') . "</p>";
 ?>
